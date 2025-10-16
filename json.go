043421a3e3c16adf62375f3927be1f69bdf84836
Vedran Kopanja <vedran@kopanja.rs>
2025-10-16 22:29:56 +0200

    Security fixes

--- json.go
+++ json.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -30,5 +31,8 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+	_, err = w.Write(dat)
+	if err != nil {
+		panic(fmt.Sprintf("there was an error writing to header: %s", err.Error()))
+	}
 }
